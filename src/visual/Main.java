8595bf3c735abe4839ecf52588951b80280d444b
penko-outlov <penko.outlov@gmail.com>
2018-02-01 08:38:37 +0200

    Add project structure

---
+++ src/visual/Main.java
@@ -0,0 +1,10 @@
+package visual;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+        });
+    }
+}
