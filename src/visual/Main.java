584042a15f11c7e4bf3cf4d8c414d482c94ad1fd
penko-outlov <penko.outlov@gmail.com>
2018-02-27 14:57:23 +0200

    Add navigation buttons to the bottom of the panel

--- src/visual/Main.java
+++ src/visual/Main.java
@@ -17,11 +17,6 @@ public class Main {
             return;
         }
 
-        DatabaseConnector.insertDepartment(new Department(4, 1, "Moscow"));
-
-        TableModel depTable = DatabaseConnector.getDepartmentModel();
-        System.out.println(depTable.getValueAt(3, 2));
-
         SwingUtilities.invokeLater(() -> {
             new Window();
         });
--- src/visual/Window.java
+++ src/visual/Window.java
@@ -2,23 +2,43 @@ package visual;
 
 import database.DatabaseConnector;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
 
 public class Window extends JFrame {
     public static final int WINDOW_WIDTH = 1000;
     public static final int WINDOW_HEIGHT = WINDOW_WIDTH / 4 * 3;
     public static final String NAME = "Task Management";
 
-    private TaskPanel testModel = new TaskPanel(DatabaseConnector.getEmployeeModel());
+    private JPanel navigationPanel = new JPanel();
+    private JButton next = new JButton("Next");
+    private JButton previous = new JButton("Previous");
 
     public Window() {
         setTitle(NAME);
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new GridLayout(2, 1));
         setLocationRelativeTo(null);
         setVisible(true);
         setResizable(false);
 
-        add(testModel);
+        makeNavigationPanel();
+    }
+
+    public void makeNavigationPanel() {
+        navigationPanel.setLayout(new GridLayout(1, 2));
+
+        previous.addActionListener((e) -> {
+        });
+        navigationPanel.add(previous);
+
+        next.addActionListener((e) -> {
+        });
+        navigationPanel.add(next);
+
+        add(navigationPanel);
     }
 }
